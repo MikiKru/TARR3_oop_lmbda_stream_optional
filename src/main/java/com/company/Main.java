27a68447c93e29dd2a7b7fb8d69301e0b8283ecf
MikiKru <michal_kruczkowski@o2.pl>
2020-05-16 10:36:40 +0200

    oop_lambda v1

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import com.company.model.Role;
+import com.company.model.User;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        User u1 = new User();
+        User u2 = new User(
+                "Michał",
+                "Kruczkowski",
+                "mk@mk.pl",
+                "mk"
+        );
+        System.out.println(u1.toString());
+        System.out.println(u2);
+        // edycja roli użytkownika
+        u2.setRole(Role.ROLE_ADMIN);
+        System.out.println("Nowa rola: " + u2.getRole());
+        System.out.println(u2);
     }
 }
--- src/main/java/com/company/model/User.java
+++ src/main/java/com/company/model/User.java
@@ -1,7 +1,7 @@
 package com.company.model;
 
 import java.time.LocalDateTime;
-
+// ALT + Insert
 // Klasa modelu -> determinuje strukturę danych w projekcie
 public class User {
     // prywatne pola klasowe ->
@@ -13,4 +13,72 @@ public class User {
     private boolean status = true;
     // przypisanie uprawnień
     private Role role = Role.ROLE_USER;
+    // konstruktor domyślny
+    public User() { }
+    // konstruktor z argumentami
+    public User(String name,
+                String lastName,
+                String email,
+                String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+    // metoda -> napisowa reprezentacja obiektu
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", status=" + status +
+                ", role=" + role +
+                '}';
+    }
+    // getters & setters -> metody dostępowe do pól klasowych
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
