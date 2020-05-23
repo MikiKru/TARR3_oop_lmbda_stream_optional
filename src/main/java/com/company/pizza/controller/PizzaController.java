5c9adb55e633c27de4f0c18fe8f7c47b859abeae
MikiKru <michal_kruczkowski@o2.pl>
2020-05-23 09:54:50 +0200

    pizza v2

--- src/main/java/com/company/pizza/controller/PizzaController.java
+++ src/main/java/com/company/pizza/controller/PizzaController.java
@@ -4,12 +4,21 @@ import com.company.pizza.model.Ingredient;
 import com.company.pizza.model.Pizza;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 // klasa kontrolera -> obsługa żądań i implementacja logiki biznesowej
 public class PizzaController {
     // Wyszukaj i zwróć najtańszą pizze ostrą
     public Pizza findCheapestSpicy(){
         return null;
+    }
+    // Wyszukaj najtańszą pizze
+    public Pizza findCheapest(){
+        return Arrays.stream(Pizza.values())                                 // Stream<Pizza>
+                .sorted(Comparator.comparing(pizza -> getPizzaPrice(pizza))) // Stream<Pizza>
+                .limit(1)                                                    // Stream<Pizza>
+                .findFirst()                                                 // Optional<Pizza>
+                .get();                                                      // Pizza
     }
     // Metoda zwracająca cenę pizzy podanej w argumencie metody
     public double getPizzaPrice(Pizza pizza){
@@ -21,7 +30,8 @@ public class PizzaController {
 
     public static void main(String[] args) {
         PizzaController pc = new PizzaController();
-        System.out.println(pc.getPizzaPrice(Pizza.MARGHERITA));
+        System.out.println("Najtańsza pizza: " + pc.findCheapest());
+
 //        System.out.println("Najtańsza pizza ostra to: " + pc.findCheapestSpicy());
     }
 }
