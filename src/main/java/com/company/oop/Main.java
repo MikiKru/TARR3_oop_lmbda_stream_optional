2a670f459411391551bfac7e430dcf5e22f78d51
MikiKru <michal_kruczkowski@o2.pl>
2020-05-23 13:47:23 +0200

    pizza v11

---
+++ src/main/java/com/company/oop/Main.java
@@ -0,0 +1,13 @@
+package com.company.oop;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        ArrayList<String> names2 = new ArrayList<>();
+
+    }
+}
