90be0e7766e16d4657b6e72e1b39cfbbe1b4f2c4
MikiKru <michal_kruczkowski@o2.pl>
2020-05-23 10:55:22 +0200

    pizza v4

--- src/main/java/com/company/pizza/controller/PizzaController.java
+++ src/main/java/com/company/pizza/controller/PizzaController.java
@@ -5,16 +5,18 @@ import com.company.pizza.model.Pizza;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // klasa kontrolera -> obsługa żądań i implementacja logiki biznesowej
 public class PizzaController {
     // Wyszukaj i zwróć najtańszą pizze ostrą
     public Pizza findCheapestSpicy() {
-        return Arrays.stream(Pizza.values())                                    // Stream<Pizza>
+        return Arrays.stream(Pizza.values())                                // Stream<Pizza>
                 .filter(pizza -> pizza.getIngredients()
                         .stream()
                         .anyMatch(ingredient -> ingredient.isSpicy()))      // Stream<Pizza>
-                .min(Comparator.comparing(this::getPizzaPrice))     // Stream<Pizza>                                            // Optional<Pizza>
+                .min(Comparator.comparing(pizza -> getPizzaPrice(pizza)))   // Stream<Pizza>                                            // Optional<Pizza>
                 .get();
     }
     // Wyszukaj najtańszą pizze
@@ -30,12 +32,36 @@ public class PizzaController {
                                 .mapToDouble(Ingredient::getPrice)   // Double
                                 .sum();         // Double
     }
+    // Wyszykaj i zwróć najdroższą pizze wegetariańską
+    Pizza findMostExpensiveVegetarian(){
+        return Arrays.stream(Pizza.values())
+                    .filter(pizza -> pizza.getIngredients()
+                                            .stream()
+                                            .noneMatch(Ingredient::isMeat))
+                    .max(Comparator.comparing(pizza -> getPizzaPrice(pizza)))   // Stream<Pizza>                                            // Optional<Pizza>
+                    .get();
+    }
+    // metoda zwracająca pizze mięsne posortowane malejąco po liczbie składników
+    public List<Pizza> iLikeMeat(){
+        return Arrays.stream(Pizza.values())
+                        .filter(pizza -> pizza.getIngredients()
+                                .stream()
+                                .anyMatch(Ingredient::isMeat))
+                        .sorted(Comparator.comparing(pizza -> pizza.getIngredients()
+                                .stream()
+                                .filter(Ingredient::isMeat)
+                                .count(), Comparator.reverseOrder()))
+                        .collect(Collectors.toList());
+    }
 
     public static void main(String[] args) {
         PizzaController pc = new PizzaController();
         System.out.println("Najtańsza pizza: " + pc.findCheapest());
         System.out.println("Najtańsza pizza ostra: " + pc.findCheapestSpicy());
-
-//        System.out.println("Najtańsza pizza ostra to: " + pc.findCheapestSpicy());
+        System.out.println("Najdroższa pizza wegetariańska to: " + pc.findMostExpensiveVegetarian());
+        pc.iLikeMeat().forEach(pizza -> System.out.println(
+                pizza + " " +
+                pizza.getIngredients().size() + " " +
+                pizza.getIngredients().stream().filter(Ingredient::isMeat).count()));
     }
 }
